0268277176d7e85e062dcfadc46d1f0045e5ea8f
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2023-02-25 16:33:42 +0700

    feat: add more flexibility

--- togift.py
+++ togift.py
@@ -24,31 +24,48 @@ qno = 1
 choices = {}
 invalid = []
 for i, line in enumerate(f):
-    if re.match(r"^[A-Z]+\. ", line):
+    if re.match(r"^[A-Za-z]+\..*?", line):
         mode = "choices"
         qline = 0
-    elif re.match(r"^ANSWER:", line):
+    elif re.match(r"ANS.*?:", line):
         mode = "answer"
         qline = 0
     else:
-        if (mode == "question" or mode == "choices") and qline == 0 and qno > 1:
+        if mode == "choices":
             invalid.append(qno-1)
-        elif mode == "question" and qno > 1:
+        elif mode == "question" and qline == 0 and line.strip() == "":
+            continue
+        elif mode == "question" and qline >= 1:
             write("\\n")
         mode = "question"
         choices = {}
 
     if mode == "question":
+        text = line.strip()
+        if qline == 0 and text == "":
+            continue
         if qline == 0:
-            write(f"::Q{qno}:: ", end="", strip=False)
+            number = str(qno).zfill(2)
+            write(f"::Q{number}:: ", end="", strip=False)
             qno += 1
-        write(f"{line}", end="")
+        match = re.search(r"^(\d+)\.(.*)", text)
+        if match:
+            text = match.group(2)
+        elif qline == 0 and text != "":
+            print(f"Possibly invalid question on line {i+1} question no {qno-1} (no numbering)")
+        write(f"{text}", end="")
         qline += 1
     elif mode == "choices":
         letter = line[0]
         choices[letter] = line[3:]
     elif mode == "answer":
-        answer = line[7:].strip()
+        match = re.search(r"\w+.*?:.*([A-Z]+)", line)
+        answer = ""
+        if match:
+            answer = match.group(1)
+        else:
+            invalid.append(qno-1)
+            print(f"Invalid answer on line {i+1}")
         write(" {", strip=False)
         for letter, choice in choices.items():
             if letter == answer:
@@ -56,6 +73,8 @@ for i, line in enumerate(f):
             else:
                 write(f"~{choice}")
         write("}\n", strip=False)
+        mode = "question"
+        qline = 0
     pass
 for i in invalid:
     print(f"Invalid question: {i}")
